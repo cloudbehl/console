e1d4934576b32e21e547d35e1a5e6807f66e33e6
Ankush Behl <cloudbehl@gmail.com>
2019-03-28 16:46:18 +0530

    Integrated OCS health card and capacity cards

--- frontend/public/kubevirt/components/cluster/cluster-overview.jsx
+++ frontend/public/kubevirt/components/cluster/cluster-overview.jsx
@@ -11,7 +11,13 @@ import {
   formatNetTraffic,
 } from 'kubevirt-web-ui-components';
 
-import { NodeModel, PodModel, PersistentVolumeClaimModel, VirtualMachineModel, InfrastructureModel } from '../../../models';
+import {
+  NodeModel,
+  PodModel,
+  PersistentVolumeClaimModel,
+  VirtualMachineModel,
+  InfrastructureModel,
+} from '../../../models';
 import { WithResources } from '../utils/withResources';
 import { k8sBasePath } from '../../module/okdk8s';
 import { coFetch, coFetchJSON } from '../../../co-fetch';
@@ -20,28 +26,35 @@ import { EventStream } from '../../../components/events';
 import { EventsInnerOverview } from './events-inner-overview';
 import { LoadingInline } from '../utils/okdutils';
 
-const CONSUMERS_CPU_QUERY = 'sort(topk(10, sum by (pod_name)(container_cpu_usage_seconds_total{pod_name!=""})))';
-const CONSUMERS_MEMORY_QUERY = 'sort(topk(10, sum by (pod_name)(container_memory_usage_bytes{pod_name!=""})))';
+const CONSUMERS_CPU_QUERY =
+  'sort(topk(10, sum by (pod_name)(container_cpu_usage_seconds_total{pod_name!=""})))';
+const CONSUMERS_MEMORY_QUERY =
+  'sort(topk(10, sum by (pod_name)(container_memory_usage_bytes{pod_name!=""})))';
 const OPENSHIFT_VERSION_QUERY = 'openshift_build_info{job="apiserver"}';
 
 const CAPACITY_CPU_TOTAL_QUERY = 'sum(kube_node_status_capacity_cpu_cores)';
-const CAPACITY_CPU_USED_QUERY = 'sum(kube_node_status_capacity_cpu_cores) - sum(kube_node_status_allocatable_cpu_cores)';
-const CAPACITY_MEMORY_TOTAL_QUERY = 'sum(kube_node_status_capacity_memory_bytes)';
-const CAPACITY_MEMORY_USED_QUERY = 'sum(kube_node_status_capacity_memory_bytes) - sum(kube_node_status_allocatable_memory_bytes)';
+const CAPACITY_CPU_USED_QUERY =
+  'sum(kube_node_status_capacity_cpu_cores) - sum(kube_node_status_allocatable_cpu_cores)';
+const CAPACITY_MEMORY_TOTAL_QUERY =
+  'sum(kube_node_status_capacity_memory_bytes)';
+const CAPACITY_MEMORY_USED_QUERY =
+  'sum(kube_node_status_capacity_memory_bytes) - sum(kube_node_status_allocatable_memory_bytes)';
 
 const CAPACITY_STORAGE_TOTAL_QUERY = 'sum(kubelet_volume_stats_capacity_bytes)'; // available with Ceph
 const CAPACITY_STORAGE_USED_QUERY = 'sum(kubelet_volume_stats_used_bytes)';
 const CAPACITY_STORAGE_TOTAL_DEFAULT_QUERY = 'sum(node_filesystem_avail_bytes)';
-const CAPACITY_STORAGE_USED_DEFAULT_QUERY = 'sum(node_filesystem_avail_bytes) - sum(node_filesystem_free_bytes)';
+const CAPACITY_STORAGE_USED_DEFAULT_QUERY =
+  'sum(node_filesystem_avail_bytes) - sum(node_filesystem_free_bytes)';
 
-const CAPACITY_NETWORK_TOTAL_QUERY = 'sum(avg by(instance)(node_network_speed_bytes))'; // TODO: needs to be refined
+const CAPACITY_NETWORK_TOTAL_QUERY =
+  'sum(avg by(instance)(node_network_speed_bytes))'; // TODO: needs to be refined
 const CAPACITY_NETWORK_USED_QUERY = 'sum(node:node_net_utilisation:sum_irate)';
 
 const REFRESH_TIMEOUT = 5000;
 
 const resourceMap = {
   nodes: {
-    resource: getResource(NodeModel, {namespaced: false}),
+    resource: getResource(NodeModel, { namespaced: false }),
   },
   pods: {
     resource: getResource(PodModel),
@@ -53,7 +66,11 @@ const resourceMap = {
     resource: getResource(VirtualMachineModel),
   },
   infrastructure: {
-    resource: getResource(InfrastructureModel, { namespaced: false, name: 'cluster', isList: false }),
+    resource: getResource(InfrastructureModel, {
+      namespaced: false,
+      name: 'cluster',
+      isList: false,
+    }),
   },
 };
 
@@ -95,10 +112,17 @@ const getInventoryData = resources => {
   };
 };
 
-const OverviewEventStream = () => <EventStream scrollableElementId="events-body" InnerComponent={EventsInnerOverview} overview={true} namespace={undefined} />;
+const OverviewEventStream = () => (
+  <EventStream
+    scrollableElementId="events-body"
+    InnerComponent={EventsInnerOverview}
+    overview={true}
+    namespace={undefined}
+  />
+);
 
 export class ClusterOverview extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.messages = {};
 
@@ -106,6 +130,7 @@ export class ClusterOverview extends React.Component {
       healthData: {
         data: {},
         loaded: false,
+        heading: 'Cluster Health',
       },
       consumersData: {
         metrics: {
@@ -144,7 +169,9 @@ export class ClusterOverview extends React.Component {
 
     this.setConsumersData = this._setConsumersData.bind(this);
     this.setHealthData = this._setHealthData.bind(this);
-    this.setDetailsOpenshiftResponse = this._setDetailsOpenshiftResponse.bind(this);
+    this.setDetailsOpenshiftResponse = this._setDetailsOpenshiftResponse.bind(
+      this
+    );
     this.setCapacityData = this._setCapacityData.bind(this);
   }
 
@@ -153,7 +180,7 @@ export class ClusterOverview extends React.Component {
     this.setState(state => ({
       consumersData: {
         metrics: {
-          ...(_.get(state.consumersData, 'metrics', {})),
+          ..._.get(state.consumersData, 'metrics', {}),
           [key]: {
             title,
             consumers: result.map(r => ({
@@ -217,22 +244,30 @@ export class ClusterOverview extends React.Component {
   fetchPrometheusQuery(query, callback) {
     const promURL = window.SERVER_FLAGS.prometheusBaseURL;
     const url = `${promURL}/api/v1/query?query=${encodeURIComponent(query)}`;
-    coFetchJSON(url).then(result => {
-      if (this._isMounted) {
-        callback(result);
-      }
-    }).then(() => {
-      if (this._isMounted) {
-        setTimeout(() => this.fetchPrometheusQuery(query, callback), REFRESH_TIMEOUT);
-      }
-    });
+    coFetchJSON(url)
+      .then(result => {
+        if (this._isMounted) {
+          callback(result);
+        }
+      })
+      .then(() => {
+        if (this._isMounted) {
+          setTimeout(
+            () => this.fetchPrometheusQuery(query, callback),
+            REFRESH_TIMEOUT
+          );
+        }
+      });
   }
-
 
   fetchHealth(callback) {
     coFetch(`${k8sBasePath}/healthz`)
       .then(response => response.text())
-      .then(text => text === 'ok' && this._isMounted ? callback(true, 'All systems healthy') : callback(false, text))
+      .then(text =>
+        text === 'ok' && this._isMounted
+          ? callback(true, 'All systems healthy')
+          : callback(false, text)
+      )
       .catch(() => {
         if (this._isMounted) {
           callback(false, 'Cannot get cluster health');
@@ -248,22 +283,48 @@ export class ClusterOverview extends React.Component {
   componentDidMount() {
     this._isMounted = true;
 
-    this.fetchPrometheusQuery(CONSUMERS_CPU_QUERY, response => this.setConsumersData('cpu', 'CPU', response));
-    this.fetchPrometheusQuery(CONSUMERS_MEMORY_QUERY, response => this.setConsumersData('memory', 'Memory', response));
+    this.fetchPrometheusQuery(CONSUMERS_CPU_QUERY, response =>
+      this.setConsumersData('cpu', 'CPU', response)
+    );
+    this.fetchPrometheusQuery(CONSUMERS_MEMORY_QUERY, response =>
+      this.setConsumersData('memory', 'Memory', response)
+    );
 
     this.fetchHealth(this.setHealthData);
-    this.fetchPrometheusQuery(OPENSHIFT_VERSION_QUERY, response => this.setDetailsOpenshiftResponse(response));
+    this.fetchPrometheusQuery(OPENSHIFT_VERSION_QUERY, response =>
+      this.setDetailsOpenshiftResponse(response)
+    );
 
-    this.fetchPrometheusQuery(CAPACITY_CPU_TOTAL_QUERY, response => this.setCapacityData('cpu', 'total', response));
-    this.fetchPrometheusQuery(CAPACITY_CPU_USED_QUERY, response => this.setCapacityData('cpu', 'used', response));
-    this.fetchPrometheusQuery(CAPACITY_MEMORY_TOTAL_QUERY, response => this.setCapacityData('memory', 'total', response));
-    this.fetchPrometheusQuery(CAPACITY_MEMORY_USED_QUERY, response => this.setCapacityData('memory', 'used', response));
-    this.fetchPrometheusQuery(CAPACITY_STORAGE_TOTAL_QUERY, response => this.setCapacityData('storage', 'total', response));
-    this.fetchPrometheusQuery(CAPACITY_STORAGE_USED_QUERY, response => this.setCapacityData('storage', 'used', response));
-    this.fetchPrometheusQuery(CAPACITY_STORAGE_TOTAL_DEFAULT_QUERY, response => this.setCapacityData('storage', 'totalDefault', response));
-    this.fetchPrometheusQuery(CAPACITY_STORAGE_USED_DEFAULT_QUERY, response => this.setCapacityData('storage', 'usedDefault', response));
-    this.fetchPrometheusQuery(CAPACITY_NETWORK_TOTAL_QUERY, response => this.setCapacityData('network', 'total', response));
-    this.fetchPrometheusQuery(CAPACITY_NETWORK_USED_QUERY, response => this.setCapacityData('network', 'used', response));
+    this.fetchPrometheusQuery(CAPACITY_CPU_TOTAL_QUERY, response =>
+      this.setCapacityData('cpu', 'total', response)
+    );
+    this.fetchPrometheusQuery(CAPACITY_CPU_USED_QUERY, response =>
+      this.setCapacityData('cpu', 'used', response)
+    );
+    this.fetchPrometheusQuery(CAPACITY_MEMORY_TOTAL_QUERY, response =>
+      this.setCapacityData('memory', 'total', response)
+    );
+    this.fetchPrometheusQuery(CAPACITY_MEMORY_USED_QUERY, response =>
+      this.setCapacityData('memory', 'used', response)
+    );
+    this.fetchPrometheusQuery(CAPACITY_STORAGE_TOTAL_QUERY, response =>
+      this.setCapacityData('storage', 'total', response)
+    );
+    this.fetchPrometheusQuery(CAPACITY_STORAGE_USED_QUERY, response =>
+      this.setCapacityData('storage', 'used', response)
+    );
+    this.fetchPrometheusQuery(CAPACITY_STORAGE_TOTAL_DEFAULT_QUERY, response =>
+      this.setCapacityData('storage', 'totalDefault', response)
+    );
+    this.fetchPrometheusQuery(CAPACITY_STORAGE_USED_DEFAULT_QUERY, response =>
+      this.setCapacityData('storage', 'usedDefault', response)
+    );
+    this.fetchPrometheusQuery(CAPACITY_NETWORK_TOTAL_QUERY, response =>
+      this.setCapacityData('network', 'total', response)
+    );
+    this.fetchPrometheusQuery(CAPACITY_NETWORK_USED_QUERY, response =>
+      this.setCapacityData('network', 'used', response)
+    );
   }
 
   componentWillUnmount() {
@@ -280,7 +341,10 @@ export class ClusterOverview extends React.Component {
             ...this.state.detailsData,
           },
           inventoryData: getInventoryData(resources), // k8s object loaded via WithResources
-          healthData: this.state.healthData,
+          healthData: {
+            LoadingComponent: LoadingInline,
+            ...this.state.healthData,
+          },
           capacityStats: {
             LoadingComponent: LoadingInline,
             ...this.state.capacityStats,
@@ -299,9 +363,11 @@ export class ClusterOverview extends React.Component {
       };
     };
 
-
     return (
-      <WithResources resourceMap={resourceMap} resourceToProps={inventoryResourceMapToProps}>
+      <WithResources
+        resourceMap={resourceMap}
+        resourceToProps={inventoryResourceMapToProps}
+      >
         <ClusterOverviewContext.Provider>
           <KubevirtClusterOverview />
         </ClusterOverviewContext.Provider>
--- frontend/public/storage/components/storage-overview/storage-overview.jsx
+++ frontend/public/storage/components/storage-overview/storage-overview.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import * as _ from 'lodash-es';
+
 import {
   StorageOverview as KubevirtStorageOverview,
   ClusterOverviewContext,
   getResource,
+  formatBytes,
+  complianceData,
 } from 'kubevirt-web-ui-components';
 
 import {
@@ -11,30 +15,37 @@ import {
   PodModel,
   PersistentVolumeClaimModel,
   VirtualMachineModel,
-  InfrastructureModel,
 } from '../../../models';
 import { WithResources } from '../../../kubevirt/components/utils/withResources';
 import { LoadingInline } from '../../../kubevirt/components/utils/okdutils';
+import { coFetchJSON } from '../../../co-fetch';
+import { EventStream } from '../../../components/events';
+import { EventsInnerOverview } from '../../../kubevirt/components/cluster/events-inner-overview';
+
+const CEPH_STATUS = 'ceph_health_status';
+const CAPACITY_STORAGE_TOTAL_QUERY = 'ceph_cluster_total_bytes'; // available with Ceph
+const CAPACITY_STORAGE_USED_QUERY = 'ceph_cluster_total_used_bytes';
+const REQUESTED_CAPACITY_STORAGE_QUERY =
+  'sum(kube_persistentvolumeclaim_resource_requests_storage_bytes)'; // available with Ceph
+const ALLOCATED_CAPACITY_STORAGE_QUERY = 'sum(kubelet_volume_stats_used_bytes)';
+
+const CEPH_ROOK_NAMESPACE = 'openshift-storage';
+const REFRESH_TIMEOUT = 30000;
 
 const resourceMap = {
   nodes: {
     resource: getResource(NodeModel, { namespaced: false }),
   },
   pods: {
-    resource: getResource(PodModel),
+    resource: getResource(PodModel, {
+      namespace: CEPH_ROOK_NAMESPACE,
+    }),
   },
   pvcs: {
     resource: getResource(PersistentVolumeClaimModel),
   },
   vms: {
     resource: getResource(VirtualMachineModel),
-  },
-  infrastructure: {
-    resource: getResource(InfrastructureModel, {
-      namespaced: false,
-      name: 'cluster',
-      isList: false,
-    }),
   },
   cephCluster: {
     resource: getResource(CephClusterModel),
@@ -71,7 +82,6 @@ const getInventoryData = resources => {
       kind: VirtualMachineModel.kind,
     };
   }
-
   return {
     inventory,
     loaded: !!inventory,
@@ -79,10 +89,134 @@ const getInventoryData = resources => {
   };
 };
 
+const OverviewEventStream = () => (
+  <EventStream
+    scrollableElementId="events-body"
+    InnerComponent={EventsInnerOverview}
+    overview={true}
+    // namespace={"openshift-storage"}
+    textFilter={'ceph.rook'}
+  />
+);
+
 export class StorageOverview extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      healthData: {
+        data: {},
+        loaded: false,
+        heading: 'OCS Health',
+      },
+      capacityStats: {
+        stats: {
+          totalCapacity: {
+            title: 'Total Capacity',
+            data: {},
+            formatValue: formatBytes,
+          },
+          requestedCapacity: {
+            title: 'Requested Capacity',
+            data: {},
+            formatValue: formatBytes,
+          },
+        },
+      },
+    };
+
+    this.setHealthData = this._setHealthData.bind(this);
+    this.setCapacityData = this._setCapacityData.bind(this);
+  }
+
+  _setHealthData(healthy, message) {
+    this.setState({
+      healthData: {
+        data: {
+          healthy,
+          message,
+        },
+        loaded: true,
+      },
+    });
+  }
+
+  _setCapacityData(key, dataKey, response) {
+    const result = response.data.result;
+    this.setState(state => {
+      const capacityStats = {
+        stats: _.get(state.capacityStats, 'stats', {}),
+      };
+
+      const value = Number(_.get(result, '[0].value[1]'));
+      if (!Number.isNaN(value)) {
+        if (dataKey === 'totalDefault') {
+          if (isNaN(capacityStats.stats[key].data.total)) {
+            capacityStats.stats[key].data.total = value;
+          }
+        } else if (dataKey === 'usedDefault') {
+          if (isNaN(capacityStats.stats[key].data.used)) {
+            capacityStats.stats[key].data.used = value;
+          }
+        } else {
+          capacityStats.stats[key].data[dataKey] = value;
+        }
+      }
+      return { capacityStats };
+    });
+  }
+
+  fetchPrometheusQuery(query, callback) {
+    const promURL = window.SERVER_FLAGS.prometheusBaseURL;
+    const url = `${promURL}/api/v1/query?query=${encodeURIComponent(query)}`;
+    coFetchJSON(url)
+      .then(result => {
+        if (this._isMounted) {
+          callback(result);
+        }
+      })
+      .then(() => {
+        if (this._isMounted) {
+          setTimeout(
+            () => this.fetchPrometheusQuery(query, callback),
+            REFRESH_TIMEOUT
+          );
+        }
+      });
+  }
+
+  fetchHealth(response, callback) {
+    const result = response.data.result;
+    result.map(r =>
+      r.value[1] === '0'
+        ? callback(true, 'OCS is healthy')
+        : callback(false, 'OCS is Unhealthy')
+    );
+  }
+
+  componentDidMount() {
+    this._isMounted = true;
+
+    this.fetchPrometheusQuery(CEPH_STATUS, response =>
+      this.fetchHealth(response, this.setHealthData)
+    );
+
+    this.fetchPrometheusQuery(CAPACITY_STORAGE_TOTAL_QUERY, response =>
+      this.setCapacityData('totalCapacity', 'totalDefault', response)
+    );
+    this.fetchPrometheusQuery(CAPACITY_STORAGE_USED_QUERY, response =>
+      this.setCapacityData('totalCapacity', 'usedDefault', response)
+    );
+
+    this.fetchPrometheusQuery(REQUESTED_CAPACITY_STORAGE_QUERY, response =>
+      this.setCapacityData('requestedCapacity', 'totalDefault', response)
+    );
+    this.fetchPrometheusQuery(ALLOCATED_CAPACITY_STORAGE_QUERY, response =>
+      this.setCapacityData('requestedCapacity', 'usedDefault', response)
+    );
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
   }
 
   render() {
@@ -94,7 +228,22 @@ export class StorageOverview extends React.Component {
             storageCluster: resources.cephCluster,
             ...this.state.detailsData,
           },
-          inventoryData: getInventoryData(resources), // k8s object loaded via WithResources
+          inventoryData: getInventoryData(resources),
+          healthData: {
+            clusterHealth: this.state.healthData,
+            LoadingComponent: LoadingInline,
+            ...this.state.healthData,
+          },
+          eventsData: {
+            Component: OverviewEventStream,
+            heading: 'OCS Events',
+            loaded: true,
+          },
+          capacityStats: {
+            LoadingComponent: LoadingInline,
+            ...this.state.capacityStats,
+          },
+          complianceData,
         },
       };
     };
